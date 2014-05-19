a777620a30a30eedf8e38011f2195fa181fde00a
Luo Haoran <1910567646@qq.com>
2014-05-19 11:51:38 +0800

    Where should the synchronization lock be placed?

--- src/net/aegistudio/lwjgl/graphic/Canvas.java
+++ src/net/aegistudio/lwjgl/graphic/Canvas.java
@@ -18,38 +18,32 @@ public abstract class Canvas implements Drawable
 		if(!Display.isCreated()) throw new GraphicIllegalStateException("Could not initialize canvas, the lwjgl OpenGL context has not yet been created.");;
 	}
 	
-	public boolean registerDrawable(Drawable drawable) throws GraphicIllegalStateException
+	public synchronized boolean registerDrawable(Drawable drawable) throws GraphicIllegalStateException
 	{
 		if(!Display.isCreated()) throw new GraphicIllegalStateException("Could not register drawable, the lwjgl OpenGL context has not yet been created.");
 		drawable.onInitialize(this);
 		return this.drawable.add(drawable);
 	}
 	
-	public boolean unregisterDrawable(Drawable drawable) throws GraphicIllegalStateException
+	public synchronized boolean unregisterDrawable(Drawable drawable) throws GraphicIllegalStateException
 	{
 		if(!Display.isCreated()) throw new GraphicIllegalStateException("Could not unregister drawable, the lwjgl OpenGL context has not yet been created.");
 		drawable.onTerminate(this);
 		return this.drawable.remove(drawable);
 	}
 	
-	public void onRefresh(Canvas canvas) throws GraphicIllegalStateException
+	public synchronized void onRefresh(Canvas canvas) throws GraphicIllegalStateException
 	{
 		if(!Display.isCreated()) throw new GraphicIllegalStateException("Could not refresh canvas, the lwjgl OpenGL context has not yet been created.");
-		synchronized(this.drawable)
-		{
-			int drawablesize = this.drawable.size();
-			for(int i = 0; i < drawablesize; i++) this.drawable.get(i).onRefresh(this);
-		}
+		int drawablesize = this.drawable.size();
+		for(int i = 0; i < drawablesize; i++) this.drawable.get(i).onRefresh(this);
 	}
 	
-	public void onTerminate(Canvas canvas) throws GraphicIllegalStateException
+	public synchronized void onTerminate(Canvas canvas) throws GraphicIllegalStateException
 	{
 		if(!Display.isCreated()) throw new GraphicIllegalStateException("Could not terminate canvas, the lwjgl OpenGL context has not yet been created.");
-		synchronized(this.drawable)
-		{
-			int drawablesize = this.drawable.size();
-			for(int i = 0; i < drawablesize; i++) this.drawable.get(i).onTerminate(this);
-		}
+		int drawablesize = this.drawable.size();
+		for(int i = 0; i < drawablesize; i++) this.drawable.get(i).onTerminate(this);
 	}
 	
 }
