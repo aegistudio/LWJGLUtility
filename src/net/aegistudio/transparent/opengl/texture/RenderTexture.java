25eec9acd27efef343567351e377d5c1eecefafa
Luo Haoran <1910567646@qq.com>
2015-02-25 01:47:22 +0800

    updates.

--- src/net/aegistudio/transparent/opengl/texture/RenderTexture.java
+++ src/net/aegistudio/transparent/opengl/texture/RenderTexture.java
@@ -2,6 +2,7 @@ package net.aegistudio.transparent.opengl.texture;
 
 import java.nio.ByteBuffer;
 
+import net.aegistudio.transparent.opengl.util.EnumDataType;
 import net.aegistudio.transparent.opengl.util.EnumPixelFormat;
 import net.aegistudio.transparent.opengl.util.FrameBufferObject;
 import net.aegistudio.transparent.util.BindingFailureException;
@@ -20,17 +21,17 @@ public class RenderTexture extends Texture
 	 */
 	public RenderTexture(FrameBufferObject fbo, int width, int height, int... attachments)
 	{
-		this(fbo, EnumPixelFormat.RGBA, GL11.GL_UNSIGNED_BYTE, width, height, GL11.GL_TEXTURE_2D, attachments);
+		this(fbo, EnumPixelFormat.RGBA, EnumDataType.BYTE, width, height, GL11.GL_TEXTURE_2D, attachments);
 	}
 	
-	public RenderTexture(FrameBufferObject fbo, EnumPixelFormat pixelFormat, int pixelType, int width, int height, int texTarget, int... attachments)
+	public RenderTexture(FrameBufferObject fbo, EnumPixelFormat pixelFormat, EnumDataType pixelType, int width, int height, int texTarget, int... attachments)
 	{
 		if(fbo == null) throw new IllegalArgumentException("The rendering FBO for this texture should not be null!");
 		this.fbo = fbo;
 		
 		this.texTarget = texTarget;
 		this.pixelFormat = pixelFormat.stateId;
-		this.pixelType = pixelType;
+		this.pixelType = pixelType.inferGLType();
 		this.width = width;
 		this.height = height;
 		this.attachments = attachments;
--- src/net/aegistudio/transparent/opengl/util/EnumBufferTarget.java
+++ src/net/aegistudio/transparent/opengl/util/EnumBufferTarget.java
@@ -1,20 +1,21 @@
 package net.aegistudio.transparent.opengl.util;
 
+import org.lwjgl.opengl.ARBCopyBuffer;
+import org.lwjgl.opengl.ARBPixelBufferObject;
+import org.lwjgl.opengl.ARBUniformBufferObject;
 import org.lwjgl.opengl.ARBVertexBufferObject;
-import org.lwjgl.opengl.GL21;
 import org.lwjgl.opengl.GL30;
-import org.lwjgl.opengl.GL31;
 
 public enum EnumBufferTarget
 {
 	ARRAY(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB),
 	ELEMENT_ARRAY(ARBVertexBufferObject.GL_ELEMENT_ARRAY_BUFFER_ARB),
-	PIXEL_UNPACK(GL21.GL_PIXEL_UNPACK_BUFFER),
-	PIXEL_PACK(GL21.GL_PIXEL_PACK_BUFFER),
-	COPY_READ(GL31.GL_COPY_READ_BUFFER),
-	COPY_WRITE(GL31.GL_COPY_WRITE_BUFFER),
+	PIXEL_UNPACK(ARBPixelBufferObject.GL_PIXEL_UNPACK_BUFFER_ARB),
+	PIXEL_PACK(ARBPixelBufferObject.GL_PIXEL_PACK_BUFFER_ARB),
+	COPY_READ(ARBCopyBuffer.GL_COPY_READ_BUFFER),
+	COPY_WRITE(ARBCopyBuffer.GL_COPY_WRITE_BUFFER),
 	TRANSFORM_FEEDBACK(GL30.GL_TRANSFORM_FEEDBACK_BUFFER),
-	UNIFORM(GL31.GL_UNIFORM_BUFFER);
+	UNIFORM(ARBUniformBufferObject.GL_UNIFORM_BUFFER);
 	
 	public final int stateId;
 	
