c1b57210b07ef1af3204ad32dd0f2e502e433b7c
Luo Haoran <1910567646@qq.com>
2015-02-24 15:19:44 +0800

    Introducing VAO into the API.

---
+++ src/net/aegistudio/transparent/opengl/model/VertexArrayObject.java
@@ -0,0 +1,55 @@
+package net.aegistudio.transparent.opengl.model;
+
+import org.lwjgl.opengl.ARBVertexArrayObject;
+import org.lwjgl.opengl.GLContext;
+
+import net.aegistudio.transparent.opengl.util.FeatureNotSupportedException;
+import net.aegistudio.transparent.util.Bindable;
+import net.aegistudio.transparent.util.BindingFailureException;
+import net.aegistudio.transparent.util.Scoped;
+
+public class VertexArrayObject implements Scoped, Bindable
+{
+	protected int arrayObjectId = 0;
+	
+	public int create()
+	{
+		if(this.arrayObjectId == 0)
+		{
+			if(!GLContext.getCapabilities().GL_ARB_vertex_array_object) throw new FeatureNotSupportedException("vertex array object");
+			this.arrayObjectId = ARBVertexArrayObject.glGenVertexArrays();
+			if(this.arrayObjectId == 0) throw new BindingFailureException("Fail to create space for the vertex array object!");
+		}
+		return this.arrayObjectId;
+	}
+	
+	/**
+	 * Set the current vertex array object in use.
+	 */
+	public void bind()
+	{
+		if(arrayObjectId == 0) throw new BindingFailureException("You must create the array before binding it!");
+		ARBVertexArrayObject.glBindVertexArray(arrayObjectId);
+	}
+	
+	/**
+	 * Set the current vertex array object not in use.
+	 */
+	public void unbind()
+	{
+		if(arrayObjectId == 0) throw new BindingFailureException("You must create the array before unbinding it!");
+		ARBVertexArrayObject.glBindVertexArray(0);
+	}
+	
+	/**
+	 * Remove / release the vertex array object.
+	 */
+	public void destroy()
+	{
+		if(this.arrayObjectId != 0)
+		{
+			ARBVertexArrayObject.glDeleteVertexArrays(arrayObjectId);
+			this.arrayObjectId = 0;
+		}
+	}
+}
